08a270b94f7a05f8383b03a998cd58c80c27f48d
Tika Pahadi <tika.pahadi@selu.edu>
2018-06-18 11:15:11 -0500

    Removed ng-modal

--- projects/ng-modal/src/lib/alert/alert.ts
+++
@@ -1,9 +0,0 @@
-import { Confirm } from '../confirm/confirm';
-
-export class Alert extends Confirm {
-    title = 'Alert';
-    message = '';
-    style: 'primary' | 'danger' | 'warning' | 'info' | 'success' | 'default' = 'success';
-    confirmButton: 'Okay';
-    cancelButton: '';
-}
--- projects/ng-modal/src/public_api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of ng-modal
- */
-export * from './lib/ng-modal.module';
-export * from './lib/alert/alert';
-export * from './lib/confirm/confirm';
-export * from './lib/modal/modal';
-export * from './lib/modal/modal-button';
-export * from './lib/toast/toast';
